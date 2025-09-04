6f826b6c6cd41755250ccb03052a873e95f32915
Hajar <hajar7abeeb@gmail.com>
2025-09-04 19:51:16 +0300

    added splash screen, footer, and their media queries.

--- script.js
+++ script.js
@@ -29,6 +29,7 @@ async function getData() {
 
             let data = await response.json();
             displayRecommendations(data);
+            createFooter();
         } catch (err) {
             console.error(err);
             displayError("Sorry, an error occurred while fetching data.");
@@ -133,3 +134,74 @@ function clearData() {
 }
 
 document.getElementById('clear').addEventListener('click', clearData);
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const textElement = document.querySelector('.text-reveal');
+    const text = textElement.textContent;
+    textElement.textContent = ''; 
+
+    let index = 0; 
+    function type() {
+        if (index < text.length) {
+            textElement.textContent += text.charAt(index); 
+            index++;
+            setTimeout(type, 60); 
+        }
+    }
+
+    setTimeout(type,4000); 
+});
+
+    setTimeout(() => {
+        const splashScreen = document.getElementById('splash-screen');
+        splashScreen.style.transform = 'translateY(-100%)'; 
+    }, 3100); 
+
+    function createFooter() {
+            if (document.querySelector('footer')) {
+        return; 
+    }
+    const footer = document.createElement('footer');
+
+    const subheading = document.createElement('h3');
+    subheading.classList.add('subheading');
+    subheading.textContent = 'Leave me a follow ^-^:';
+  
+
+    const nav = document.createElement('nav');
+    nav.classList.add('contacts');
+    nav.id = 'contact';
+
+    const links = [
+        {
+            href: 'https://github.com/Hajar-h',
+            imgSrc: 'media/GH.png',
+            alt: 'Github logo'
+        },
+        {
+            href: 'https://www.linkedin.com/in/hajar-habeeb-a87618284?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app',
+            imgSrc: 'media/Li.png',
+            alt: 'Linked in logo'
+        },
+
+    ];
+
+    links.forEach(link => {
+        const a = document.createElement('a');
+        a.href = link.href;
+
+        const img = document.createElement('img');
+        img.src = link.imgSrc;
+        img.alt = link.alt;
+        img.height = 30;
+
+        a.appendChild(img);
+        nav.appendChild(a);
+    });
+
+    footer.appendChild(subheading);
+    footer.appendChild(nav);
+
+    document.body.appendChild(footer); // Change to the desired container if necessary
+}
